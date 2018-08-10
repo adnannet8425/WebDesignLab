0492f4ad9eade2a327b1f0233803b1db983d57f7
CODept <codept@aiktc05>
2018-08-10 11:01:50 +0530

    Done At 10/08/2018 11:15

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
-var element = document.getElementById('about');
-console.log(element.textContent);
+$(function(){
+	$('#rollno').on('blur',function(){
+		if(!/(\d{2}((CO)|(DCO)|(EE)|(DEE)|(EX)|(DEX)|(ME)|(DME)|(CE)|(DCE)|(CES)|(DCES))\d{2,3})/i.test(this.value)){
+			alert("Invalid Roll No");
+			this.value="";
+			$(this).focus();
+		}
+	});
+
+	$('#sname').on('keypress',function(e){
+		if(/[^a-zA-Z]/.test(e.key)){
+			alert("Invalid Name. Only Alphabets and Spaces are allowed");
+			this.value="";
+			$(this).focus();
+			return false;
+		}
+	});
+
+	$('#address').on('blur',function(){
+		if(this.value.length<50 || this.value.length>150){
+			alert("Address Should be Between 50-150 Character");
+			this.value="";
+			$(this).focus();
+		}
+	});
+});
