5ae41619f70e927574234d32f028aeb94e66bbb5
CODept <codept@aiktc05>
2018-08-17 09:21:32 +0530

    Done At 17 Aug 2018

--- js/app.js
+++ js/app.js
@@ -23,4 +23,12 @@ $(function(){
 			$(this).focus();
 		}
 	});
+	
+	$('#semail').on('blur',function(){
+		if(!/(^\w+)@([a-z]+).([a-z]{2,3})/i.test(this.value)){
+			alert("Invalid Email Address.");
+			this.value="";
+			$(this).focus();
+		}
+	});
 });
